5d2b6fed706a5dac118b2337c7b3f262c60110d3
Martin Bertello <martinbertello7@gmail.com>
2021-11-21 13:35:39 -0300

    update

--- controllers/rubro.js
+++ controllers/rubro.js
@@ -2,42 +2,41 @@
 
 const express = require('express');
 const Model   = require('../models/rubro');
-const Usuario = require('../models/usuario');
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
-const JWTstrategy = require('passport-jwt').Strategy;
-const ExtractJWT = require('passport-jwt').ExtractJwt;
-const config = require("../config/index");
 const router  = express.Router();
 
+const errorResponse = (error) => {
+    return {
+        code: 500,
+        message: "Error",
+        printStackTrace: error
+    }
+}
+
+const successResponse = {
+    code: 200,
+    message: 'OK'
+}
+
+//Save new rubro
 router.post('/', (request, response) => {
     let item = new Model(request.body);
-    item.save((error, item) => {
+    item.save((error) => {
         if (error) {
-            return response.status(500).json({
-                code: '500',
-                marca: "Ha ocurrido un error",
-                stacktrace: error
-            });
+            return response.status(500).json(errorResponse(error));
         }
-
-        return response.status(201).json(item);
+        return response.status(200).json(successResponse);
     });
 });
 
+//Get rubro by id
 router.get('/:id', (request, response) => {
     Model.findById(request.params.id).exec((error, item) => {
-        if (error) {
-            return response.status(500).json({
-                code: '500',
-                marca: "Ha ocurrido un error",
-                stacktrace: error
-            });
-        }
+        if (error) return response.status(500).json(errorResponse(error));
         return response.status(200).json(item);
     });
 });
 
+//Get rubros list
 router.get('/', (request, response) => {
     const {page, limit, filters} = request.query;
     const query = JSON.parse(filters);
@@ -45,73 +44,37 @@ router.get('/', (request, response) => {
         page,
         limit,
     }, (error, items) => {
-        if (error) {
-            return response.status(500).json({
-                code: '500',
-                marca: "Ha ocurrido un error",
-                stacktrace: error
-            });
-        }
+        if (error) return response.status(500).json(errorResponse(error));
         return response.status(200).json(items);
     })
 });
 
+//Get rubros by name
 router.get('/name/:name', (request, response) => {
     console.log(request.params.name);
     Model.paginate({nombre: new RegExp(request.params.name)}, {
         page: 0,
         limit: 10,
     }, (error, items) => {
-        if (error) {
-            return response.status(500).json({
-                code: '500',
-                marca: "Ha ocurrido un error",
-                stacktrace: error
-            });
-        }
+        if (error) return response.status(500).json(errorResponse(error));
         return response.status(200).json(items);
     })
 });
 
+//Update rubro
 router.put('/:id', (request, response) => {
     let item = new Model(request.body);
-    Model.findOneAndUpdate({ _id: request.params.id }, item, { new: true }, (error, item) => {
-        if (error) {
-            return response.status(500).json({
-                code: '500',
-                marca: "Ha ocurrido un error",
-                stacktrace: error
-            });
-        }
-
-        return response.status(200).json(item);
+    Model.findOneAndUpdate({ _id: request.params.id }, item, { new: true }, (error) => {
+        if (error) return response.status(500).json(errorResponse(error));
+        return response.status(200).json(successResponse);
     });
 });
 
+//Delete rubro
 router.delete('/:id', (request, response) => {
-    Model.findById(request.params.id, function (error, item) {
-        if (error) {
-            return response.status(500).json({
-                code: '500',
-                marca: "Ha ocurrido un error",
-                stacktrace: error
-            });
-        }
-
-        item.remove((error, item) => {
-            if (error) {
-                return response.status(500).json({
-                    code: '500',
-                    marca: "Ha ocurrido un error",
-                    stacktrace: error
-                });
-            }
-
-            return response.status(204).json({
-                code: '204',
-                marca: "Se ha eliminado con éxito"
-            });
-        });
+    Model.deleteOne({_id: request.params.id}, (error) => {
+        if (error) return response.status(500).json(errorResponse(error));
+        return response.status(200).json(successResponse);
     });
 });
 
