c4610f63d94d40763ab780cb493791e64490131d
EGL-dev <egdev5340@gmail.com>
2021-12-11 14:49:21 -0300

    add crud cuentacorriente

--- controllers/cuentacorriente.js
+++ controllers/cuentacorriente.js
@@ -24,5 +24,53 @@ const successWithItems = (items) => {
 }
 
 
+router.post('/', (request, response) => {
+    let item = new Model(request.body);
+    item.save((error) => {
+        if (error) {
+            return response.status(500).json(errorResponse(error));
+        }
+        return response.status(200).json(successResponse);
+    });
+});
 
+
+router.get('/:id', (request, response) => {
+    Model.findById(request.params.id).exec((error, item) => {
+        if (error) return response.status(500).json(errorResponse(error));
+        return response.status(200).json(item);
+    });
+});
+
+
+router.get('/', (request, response) => {
+    const {page, limit, filters} = request.query;
+    const query = JSON.parse(filters);
+    Model.paginate((query) ? {nombre: new RegExp(query.nombre, 'i')} : {}, {
+        page,
+        limit,
+    }, (error, items) => {
+        if (error) return response.status(500).json(errorResponse(error));
+        return response.status(200).json(items);
+    })
+});
+
+
+router.put('/:id', (request, response) => {
+    let item = new Model(request.body);
+    Model.findOneAndUpdate({ _id: request.params.id }, item, { new: true }, (error) => {
+        if (error) return response.status(500).json(errorResponse(error));
+        return response.status(200).json(successResponse);
+    });
+});
+
+
+router.delete('/:id', (request, response) => {
+    Model.deleteOne({_id: request.params.id}, (error) => {
+        if (error) return response.status(500).json(errorResponse(error));
+        return response.status(200).json(successResponse);
+    });
+});
+
+module.exports = router;
 
