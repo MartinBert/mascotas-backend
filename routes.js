e8cac8e3852056ae448d106048c4cc3412d0f28d
EGL-dev <egdev5340@gmail.com>
2021-12-11 14:15:58 -0300

    fixed merge in routes

--- routes.js
+++ routes.js
@@ -10,12 +10,9 @@ module.exports = app => {
     router.use('/rubros', auth.verifyAuthentication, require('./controllers/rubro'));
     router.use('/marcas', auth.verifyAuthentication, require('./controllers/marca'));
     router.use('/salidas', auth.verifyAuthentication, require('./controllers/salida'));
-<<<<<<< HEAD
     router.use('/cuentacorriente', auth.verifyAuthentication, require('./controllers/cuentacorriente'));
-=======
     router.use('/documentos', auth.verifyAuthentication, require('./controllers/documento'));
     router.use('/mediospago', auth.verifyAuthentication, require('./controllers/mediopago'));
->>>>>>> 56a1eea477de2a42ab5b29dfa0b8864dfd2db893
     router.use('/uploads', auth.verifyAuthentication, require('./controllers/uploader'));
     app.use('/api/v1/', router);
 };
