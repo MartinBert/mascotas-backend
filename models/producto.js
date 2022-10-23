f04ae4347f7f42099b666e66daaa5c56e403a023
Martin Bertello <martinbertello7@gmail.com>
2022-10-23 12:37:15 -0300

    update

--- models/producto.js
+++ models/producto.js
@@ -31,43 +31,68 @@ const schema = new Schema({
     },
     cantidadFraccionadaStock: {
         type: Number,
-        required: true
+        required: true,
+        default: 0
     },
     cantidadStock: {
         type: Number,
-        required: true
+        required: true,
+        default: 0
     },
     precioUnitario: {
         type: Number,
-        required: true
+        required: true,
+        default: 0
     },
     margenGanancia: {
         type: Number,
-        required: true
+        required: true,
+        default: 0
+    },
+    margenGananciaFraccionado: {
+        type: Number,
+        required: true,
+        default: 0
     },
     precioVenta: {
         type: Number,
-        required: true
+        required: true,
+        default: 0
     },
     gananciaNeta: {
         type: Number,
-        required: true
+        required: true,
+        default: 0
+    },
+    precioVentaFraccionado: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    gananciaNetaFraccionado: {
+        type: Number,
+        required: true,
+        default: 0
     },
     porcentajeIvaCompra: {
         type: Number,
-        required: true
+        required: true,
+        default: 0
     },
     ivaCompra: {
         type: Number,
-        required: true
+        required: true,
+        default: 0
     },
     porcentajeIvaVenta: {
         type: Number,
-        required: true
+        required: true,
+        default: 0
     },
     ivaVenta: {
         type: Number,
-        required: true
+        required: true,
+        default: 0
     },
     imagenes:[{
         type: mongoose.Schema.Types.ObjectId,
