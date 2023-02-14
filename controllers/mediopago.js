527c8df889a0fa4ae458e83d1a2c20aa02461091
Agustin Bertello <bertelloagustin@gmail.com>
2023-02-14 17:57:02 -0300

    update controllers/venta

--- controllers/mediopago.js
+++ controllers/mediopago.js
@@ -1,7 +1,7 @@
-'use strict';
-const express = require('express');
-const Model   = require('../models/mediopago');
-const router  = express.Router();
+'use strict'
+const express = require('express')
+const Model   = require('../models/mediopago')
+const router  = express.Router()
 
 const errorResponse = (error) => {
     return {
@@ -18,61 +18,68 @@ const successResponse = {
 
 //Save new mediopago
 router.post('/', (request, response) => {
-    let item = new Model(request.body);
+    let item = new Model(request.body)
     item.save((error) => {
         if (error) {
-            return response.status(500).json(errorResponse(error));
+            return response.status(500).json(errorResponse(error))
         }
-        return response.status(200).json(successResponse);
-    });
-});
+        return response.status(200).json(successResponse)
+    })
+})
 
 //Get mediopago by id
 router.get('/:id', (request, response) => {
     Model.findById(request.params.id).exec((error, item) => {
-        if (error) return response.status(500).json(errorResponse(error));
-        return response.status(200).json(item);
-    });
-});
+        if (error) return response.status(500).json(errorResponse(error))
+        return response.status(200).json(item)
+    })
+})
 
 //Get mediospago list
 router.get('/', (request, response) => {
-    const {page, limit, filters} = request.query;
-    const query = JSON.parse(filters);
-    Model.paginate((query) ? {nombre: new RegExp(query.nombre, 'i')} : {}, {
-        page,
-        limit,
-    }, (error, items) => {
-        if (error) return response.status(500).json(errorResponse(error));
-        return response.status(200).json(items);
-    })
-});
+    if (request.query.length > 0) {
+        const {page, limit, filters} = request.query
+        const query = JSON.parse(filters)
+        Model.paginate((query) ? {nombre: new RegExp(query.nombre, 'i')} : {}, {
+            page,
+            limit,
+        }, (error, items) => {
+            if (error) return response.status(500).json(errorResponse(error))
+            return response.status(200).json(items)
+        })
+    } else {
+        Model.find({}).exec((error, items) => {
+            if (error) return response.status(500).json(errorResponse(error))
+            return response.status(200).json(items)
+        })
+    }
+})
 
 //Get mediospago list id
 router.get('/multiple/idList', (request, response) => {
-    const {ids} = request.query;
-    const query = {_id: {$in: JSON.parse(ids)}};
+    const {ids} = request.query
+    const query = {_id: {$in: JSON.parse(ids)}}
     Model.find(query, (error, items) => {
-        if (error) return response.status(500).json(errorResponse(error));
-        return response.status(200).json(items);
+        if (error) return response.status(500).json(errorResponse(error))
+        return response.status(200).json(items)
     })
-});
+})
 
 //Update mediopago
 router.put('/:id', (request, response) => {
-    let item = new Model(request.body);
+    let item = new Model(request.body)
     Model.findOneAndUpdate({ _id: request.params.id }, item, { new: true }, (error) => {
-        if (error) return response.status(500).json(errorResponse(error));
-        return response.status(200).json(successResponse);
-    });
-});
+        if (error) return response.status(500).json(errorResponse(error))
+        return response.status(200).json(successResponse)
+    })
+})
 
 //Delete mediopago
 router.delete('/:id', (request, response) => {
     Model.deleteOne({_id: request.params.id}, (error) => {
-        if (error) return response.status(500).json(errorResponse(error));
-        return response.status(200).json(successResponse);
-    });
-});
+        if (error) return response.status(500).json(errorResponse(error))
+        return response.status(200).json(successResponse)
+    })
+})
 
-module.exports = router;
+module.exports = router
