963d2f730bb0c2a71de3278d9dd92b5c0a0ca565
root <bertelloagustin@gmail.com>
2024-10-03 04:26:39 -0300

    update 'services/certificates'.

--- config/index.js
+++ config/index.js
@@ -2,12 +2,12 @@
 
 module.exports = {
     // Local -- test
-    // port: process.env.PORT || 8000,
-    // database: 'mongodb://localhost:27017/test',
+    port: process.env.PORT || 8000,
+    database: 'mongodb://localhost:27017/test',
 
     //Production
-    port: process.env.PORT || 80,
-    database: 'mongodb://localhost:27017/tienda-mascotas',
+    // port: process.env.PORT || 80,
+    // database: 'mongodb://localhost:27017/tienda-mascotas',
     jwt: {
         secretKey: 'K{RYHwN.WC-BBxe{]AD9,CCH%02[4Yn5A%I*?4106r}kP%ySfJ%Pu?u@E,)wh:8',
         expireTime: `${Number.MAX_SAFE_INTEGER}d`
